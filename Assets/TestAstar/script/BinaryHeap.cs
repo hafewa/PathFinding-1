3e121dd04c91381c37ee5f5cd7109576c07923fc
liuning <17801090782@163.com>
2017-02-06 20:31:53 +0800

    寻路功能

---
+++ Assets/TestAstar/script/BinaryHeap.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+namespace BinaryHeap
+{
+    /// <summary>  
+    /// The heap's order  
+    /// </summary>  
+    public enum Order
+    {
+        ASC = 0,
+        DESC = 1
+    }
+
+    /// <summary>  
+    /// The BinaryHeap  
+    /// </summary>  
+    /// <typeparam name="T">The T represent the type of the heap's value</typeparam>  
+    public class BinaryHeap<T> where T : IComparable<T>
+    {
+        /// <summary>  
+        /// The size of the heap  
+        /// </summary>  
+        public int Size { get; set; }
+
+        private int length;
+
+        /// <summary>
+        /// 默认值
+        /// </summary>
+        private T defaultValue;
+        /// <summary>  
+        /// The length of the heap  
+        /// </summary>  
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+            private set { length = value; }
+        }
+
+        private T[] Items { get; set; }
+        private Order Order = Order.ASC;
+
+        /// <summary>  
+        /// The Cons of the heap  
+        /// </summary>  
+        /// <param name="size">The default size of the heap</param>  
+        /// <param name="order">The order of the heap</param>  
+        public BinaryHeap(int size, Order order)
+        {
+            if (size < 1)
+            {
+                throw new Exception("The size should be greater or equal than 1.");
+            }
+
+            this.Size = size;
+            this.Order = order;
+            // We don't need the Items[0], so the actually size is (this.Size + 1),  
+            // and we just use the the value of Items[1], Items[2], Items[3]... and so on  
+            this.Size++;
+            Items = new T[this.Size];
+
+            // Set to 0 represent the heap's length is empty  
+            this.length = 0;
+        }
+
+        /// <summary>  
+        /// Add new item to the heap  
+        /// </summary>  
+        /// <param name="item"></param>  
+        public void Push(T item)
+        {
+            if (this.length == 0)
+            {
+                Items[1] = item;
+            }
+            else
+            {
+                int len = this.length;
+                if (len + 1 >= this.Size)
+                {
+                    throw new Exception("The heap is fulfilled, can't add item anymore." + len);
+                }
+
+                // Set the new item at the end of this heap  
+                int endPos = len + 1;
+                try
+                {
+                    Items[endPos] = item;
+                }
+                catch
+                {
+                    int j = 0;
+                }
+                // Calculate the new item's parent position  
+                int parentPos = endPos / 2;
+                bool isContinue = true;
+                while (parentPos != 0 && isContinue)
+                {
+                    // Compare the new added item and its parent, swap each other if needed  
+                    if (Order == BinaryHeap.Order.ASC)
+                    {
+                        if (Items[endPos].CompareTo(Items[parentPos]) < 0)
+                        {
+                            Swap(ref Items[endPos], ref Items[parentPos]);
+
+                            endPos = parentPos;
+                            parentPos = endPos / 2;
+                        }
+                        else
+                        {
+                            isContinue = false;
+                        }
+                    }
+                    else
+                    {
+                        if (Items[endPos].CompareTo(Items[parentPos]) > 0)
+                        {
+                            Swap(ref Items[endPos], ref Items[parentPos]);
+
+                            endPos = parentPos;
+                            parentPos = endPos / 2;
+                        }
+                        else
+                        {
+                            isContinue = false;
+                        }
+                    }
+                }
+            }
+
+            // After the new item added, set the heap's length added by one  
+            this.length++;
+        }
+
+        /// <summary>  
+        /// Remove the top item from the heap  
+        /// </summary>  
+        /// <returns>if the order is ASC, return the smallest one, if DESC, return the largest one.</returns>  
+        public T Pop()
+        {
+            if (this.length == 0)
+            {
+                throw new Exception("The heap is empty");
+            }
+
+            // Remove the first item and move the last item to the first  
+            T removedItem = Items[1];
+            int len = this.length;
+            if (len == 1)
+            {
+                len = 0;
+            }
+            Items[1] = Items[len];
+            // After the top item removed, set the heap's length reduced by one  
+            this.length--;
+            // Get the removing item's childrens's position  
+            int currentPos = 1;
+            int leftChildPos = currentPos * 2;
+            int rightChildPos = currentPos * 2 + 1;
+
+            // Set the while loop continue or not flag  
+            bool isContinue = true;
+
+            while ((leftChildPos <= len || rightChildPos <= len) && isContinue)
+            {
+                // Compare the removing item to its childrens, swap each other if needed  
+                if (Order == BinaryHeap.Order.ASC)
+                {
+                    #region Order == BinaryHeap.Order.ASC  
+                    if (leftChildPos <= len && rightChildPos <= len)
+                    {
+                        if (Items[leftChildPos].CompareTo(Items[rightChildPos]) < 0 && Items[currentPos].CompareTo(Items[leftChildPos]) >= 0)
+                        {
+                            Swap(ref Items[currentPos], ref Items[leftChildPos]);
+                            currentPos = leftChildPos;
+                        }
+                        else if (Items[leftChildPos].CompareTo(Items[rightChildPos]) >= 0 && Items[currentPos].CompareTo(Items[rightChildPos]) >= 0)
+                        {
+                            Swap(ref Items[currentPos], ref Items[rightChildPos]);
+                            currentPos = rightChildPos;
+                        }
+                        else
+                        {
+                            isContinue = false;
+                        }
+                    }
+                    else if (leftChildPos <= len)
+                    {
+                        if (Items[currentPos].CompareTo(Items[leftChildPos]) >= 0)
+                        {
+                            Swap(ref Items[currentPos], ref Items[leftChildPos]);
+                            currentPos = leftChildPos;
+                        }
+                        else
+                        {
+                            isContinue = false;
+                        }
+                    }
+                    else if (rightChildPos <= len)
+                    {
+                        if (Items[currentPos].CompareTo(Items[rightChildPos]) >= 0)
+                        {
+                            Swap(ref Items[currentPos], ref Items[rightChildPos]);
+                            currentPos = rightChildPos;
+                        }
+                        else
+                        {
+                            isContinue = false;
+                        }
+                    }
+                    else
+                    {
+                        isContinue = false;
+                    }
+                    #endregion
+
+                    leftChildPos = currentPos * 2;
+                    rightChildPos = currentPos * 2 + 1;
+                }
+                else
+                {
+                    #region Order == BinaryHeap.Order.DESC  
+                    if (leftChildPos <= len && rightChildPos <= len)
+                    {
+                        if (Items[leftChildPos].CompareTo(Items[rightChildPos]) > 0 && Items[currentPos].CompareTo(Items[leftChildPos]) <= 0)
+                        {
+                            Swap(ref Items[currentPos], ref Items[leftChildPos]);
+                            currentPos = leftChildPos;
+                        }
+                        else if (Items[leftChildPos].CompareTo(Items[rightChildPos]) <= 0 && Items[currentPos].CompareTo(Items[rightChildPos]) <= 0)
+                        {
+                            Swap(ref Items[currentPos], ref Items[rightChildPos]);
+                            currentPos = rightChildPos;
+                        }
+                        else
+                        {
+                            isContinue = false;
+                        }
+                    }
+                    else if (leftChildPos <= len)
+                    {
+                        if (Items[currentPos].CompareTo(Items[leftChildPos]) <= 0)
+                        {
+                            Swap(ref Items[currentPos], ref Items[leftChildPos]);
+                            currentPos = leftChildPos;
+                        }
+                        else
+                        {
+                            isContinue = false;
+                        }
+                    }
+                    else if (rightChildPos <= len)
+                    {
+                        if (Items[currentPos].CompareTo(Items[rightChildPos]) <= 0)
+                        {
+                            Swap(ref Items[currentPos], ref Items[rightChildPos]);
+                            currentPos = rightChildPos;
+                        }
+                        else
+                        {
+                            isContinue = false;
+                        }
+                    }
+                    else
+                    {
+                        isContinue = false;
+                    }
+                    #endregion
+
+                    leftChildPos = currentPos * 2;
+                    rightChildPos = currentPos * 2 + 1;
+                }
+            }
+
+            return removedItem;
+        }
+
+        /// <summary>  
+        /// Sort the heap  
+        /// </summary>  
+        /// <returns>Return the sorted heap array</returns>  
+        public IEnumerable<T> Sort()
+        {
+            if (this.length == 0)
+            {
+                throw new Exception("The heap is empty");
+            }
+
+            while (this.length > 0)
+            {
+                yield return Pop();
+            }
+        }
+
+        /// <summary>
+        /// 判断对象是否在二叉堆中
+        /// </summary>
+        /// <param name="t">判断对象</param>
+        /// <returns>对象</returns>
+        public T Exist(T t)
+        {
+            T tmpItem;
+            for (int i = 0; i < Items.Length; i++)
+            {
+                tmpItem = Items[i];
+                if (tmpItem != null && tmpItem.CompareTo(t) == 0)
+                {
+                    Debug.Log("True");
+                    return Items[i];
+                }
+            }
+            Debug.Log("False");
+            return defaultValue;
+        }
+
+        #region Private method  
+        /// <summary>  
+        /// Swap each other  
+        /// </summary>  
+        /// <param name="t1">The first one</param>  
+        /// <param name="t2">The second one</param>  
+        private void Swap(ref T t1, ref T t2)
+        {
+            T temp = t1;
+            t1 = t2;
+            t2 = temp;
+        }
+        #endregion
+    }
+}
---
+++ Assets/TestAstar/script/BinaryHeap1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// 二叉堆
+/// </summary>
+/// <typeparam name="T">节点类型</typeparam>
+public class BinaryHeap<T> where T : IComparable<T>
+{
+    /// <summary>
+    /// 从当前节点开始的树所包含节点的数量
+    /// </summary>
+    public int Count { get; private set; }
+    /// <summary>
+    /// 当前节点
+    /// </summary>
+    public T CurrentNode { get; private set; }
+
+    /// <summary>
+    /// 是否为大顶二叉堆
+    /// false则为小顶二叉堆
+    /// </summary>
+    private bool isMaxTop = true;
+
+    /// <summary>
+    /// 父节点
+    /// </summary>
+    private BinaryHeap<T> parentNode; 
+
+    public BinaryHeap(T t, BinaryHeap<T> parentNode = null)
+    {
+        CurrentNode = t;
+        this.parentNode = parentNode;
+    }
+
+    /// <summary>
+    /// 左子节点
+    /// </summary>
+    public BinaryHeap<T> LeftNode { get; set; }
+
+    /// <summary>
+    /// 右子节点
+    /// </summary>
+    public BinaryHeap<T> RightNode { get; set; }
+
+    /// <summary>
+    /// 插入节点
+    /// 插入后整理二叉堆
+    /// </summary>
+    /// <param name="t">节点内容</param>
+    public void Push(T t)
+    {
+        // TODO 判断当前节点是否为空
+        // 对比
+        var result = CurrentNode.CompareTo(t);
+        // 如果对比大于当前节点并且是大顶二叉堆则替换当前位置
+        // 反之亦然
+        // 子节点大小排序, 根节点之下第一排开始, 从左到右从小到大, 第二排从右到左从小到大......
+        if (isMaxTop)
+        {
+            // 参数大于当前节点
+            if (result < 0)
+            {
+                // 替换当前节点位置
+                var orginNode = CurrentNode;
+                // 当前节点赋值
+                CurrentNode = t;
+                // 对比两子节点将小的那个挤到下一层
+
+                // 右侧节点为空
+                // 左侧为空右侧一定为空
+                if (RightNode != null)
+                {
+                    // 将右侧节点值取出放入左侧节点,
+                    if (LeftNode != null)
+                    {
+                        LeftNode.Push(RightNode.CurrentNode);
+                    }
+                    else
+                    {
+                        LeftNode = new BinaryHeap<T>(RightNode.CurrentNode, this);
+                    }
+                    RightNode.Push(orginNode);
+                    // 直接将当前右侧节点放到左侧节点, 左侧节点如果存在则挤到下面
+                }
+                else
+                {
+                    LeftNode = new BinaryHeap<T>(orginNode, this);
+                }
+            }
+            else
+            {
+                // 小于当前节点, 则将其插入该节点的子节点
+                // 对比同级子节点
+                //var SameLevelNodeList = GetNodeListByLevel()
+                // 使用
+            }
+        }
+        Count++;
+    }
+
+    /// <summary>
+    /// 返回最顶端的值
+    /// </summary>
+    /// <returns></returns>
+    public T Pop()
+    {
+        // 弹出最顶端值
+        var result = CurrentNode;
+        // 删除顶端节点的值
+
+        // 将子树重新填充排序
+
+
+        Count--;
+        return result;
+    }
+
+    /// <summary>
+    /// 根据层级获取该一级的所有节点
+    /// </summary>
+    /// <param name="rootNode">开始搜索的节点</param>
+    /// <param name="level">搜索从开始节点的子节点开始第几级</param>
+    /// <returns></returns>
+    public List<BinaryHeap<T>> GetNodeListByLevel(BinaryHeap<T> rootNode, int level)
+    {
+        if (rootNode == null || level < 0)
+        {
+            return null;
+        }
+
+        List<BinaryHeap<T>> result = null;
+
+        // 遍历列表缓存
+        List<BinaryHeap<T>> tmp = new List<BinaryHeap<T>>();
+        List<BinaryHeap<T>> tmp2 = new List<BinaryHeap<T>>();
+        var count = 0;
+        tmp.Add(rootNode);
+        while (count <= level)
+        {
+
+            // 搜索该层所有节点
+            foreach (var node in tmp)
+            {
+                tmp2.Add(node.LeftNode);
+                tmp2.Add(node.RightNode);
+            }
+
+            tmp.Clear();
+            tmp.AddRange(tmp2);
+
+
+            tmp2.Clear();
+            count++;
+        }
+        result = tmp;
+        return result;
+    }
+
+
+
+
+    public void Delete(T t)
+    {
+
+    }
+    
+}
