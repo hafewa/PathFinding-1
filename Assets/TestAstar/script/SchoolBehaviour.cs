8dc5465feea7862f82a5574cee2f0973138da669
liuning <17801090782@163.com>
2017-02-08 20:48:12 +0800

    组队行进

---
+++ Assets/TestAstar/script/SchoolBehaviour.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// 集群行为
+/// </summary>
+public class SchoolBehaviour : MonoBehaviour
+{
+
+    
+    /// <summary>
+    /// 组队ID, 只读
+    /// </summary>
+    public int GroupId
+    {
+        get { return groupId; }
+        set
+        {
+            // 判断是否已有group, 如果有并且ID不同, 则删除原有group中的member
+            if (value != groupId)
+            {
+                // group列表中是否有改ID的group, 如果有则插入其中, 如果没有则创建
+                group.MemberList.Remove(this);
+                var newGroup = SchoolManager.GetGroupById(groupId);
+                if (newGroup == null)
+                {
+                    newGroup = new SchoolGroup(groupId);
+                    SchoolManager.GroupList.Add(newGroup);
+                    group = newGroup;
+                }
+                groupId = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 组队编号
+    /// </summary>
+    public SchoolGroup Group {
+        get { return group; }
+    }
+
+    /// <summary>
+    /// 当前位置引用
+    /// 读取与设置的为GameObject的localPosition
+    /// </summary>
+    public Vector3 Position
+    {
+        get { return this.transform.localPosition; }
+        set { this.transform.localPosition = value; }
+    }
+    
+    /// <summary>
+    /// 开始移动时调用
+    /// </summary>
+    public Action<GameObject> Moveing { get; set; }
+
+    /// <summary>
+    /// 拥挤等待时调用
+    /// </summary>
+    public Action<GameObject> Wait { get; set; }
+
+    /// <summary>
+    /// 移动结束后调用
+    /// </summary>
+    public Action<GameObject> Complete { get; set; }
+
+    /// <summary>
+    /// 组编号
+    /// </summary>
+    private int groupId;
+
+    /// <summary>
+    /// 组对象
+    /// </summary>
+    private SchoolGroup group;
+
+
+    public SchoolBehaviour(int groupId)
+    {
+        this.GroupId = groupId;
+    }
+
+    public void Start()
+    {
+        // 将自己存入队员列表
+        Group.MemberList.Add(this);
+    }
+
+    public void Destory()
+    {
+        // 销毁时从列表中消除当前队员
+        Group.MemberList.Remove(this);
+    }
+}
+
+/// <summary>
+/// 集群编队
+/// </summary>
+public class SchoolGroup
+{
+    /// <summary>
+    /// group所属阵营
+    /// </summary>
+    public int Camp { get; set; }
+
+    /// <summary>
+    /// 目标位置
+    /// </summary>
+    public Vector3 Target { get; set; }
+
+    /// <summary>
+    /// 成员列表
+    /// </summary>
+    public List<SchoolBehaviour> MemberList = new List<SchoolBehaviour>();
+
+    /// <summary>
+    /// 组编号
+    /// </summary>
+    public int GroupId;
+
+    /// <summary>
+    /// 创建group时给予ID
+    /// </summary>
+    /// <param name="groupId"></param>
+    public SchoolGroup(int groupId)
+    {
+        GroupId = groupId;
+    }
+
+}
---
+++ Assets/TestAstar/script/SchoolManager.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// 集群管理
+/// 集群行为都在这里集中实现
+/// </summary>
+public class SchoolManager : MonoBehaviour
+{
+
+    /// <summary>
+    /// 组列表(全局)
+    /// </summary>
+    public static List<SchoolGroup> GroupList = new List<SchoolGroup>();
+
+    public void Start()
+    {
+        // 初始化
+    }
+    public void Update()
+    {
+        // 挨个更新对象位置
+        for (var i = 0; i < GroupList.Count; i++)
+        {
+            var group = GroupList[i];
+            Move(group);
+        }
+    }
+
+    /// <summary>
+    /// 组队移动
+    /// </summary>
+    /// <param name="group">组队对象</param>
+    private void Move(SchoolGroup group)
+    {
+        // 计算方向与速度
+        for (var j = 0; j < group.MemberList.Count; j++)
+        {
+            var member = group.MemberList[j];
+            var dir = group.Target - member.Position;
+            // 遍历同队队友
+            for (var k = 0; k < group.MemberList.Count; k++)
+            {
+                var otherMember = group.MemberList[k];
+                if (otherMember.Equals(member))
+                {
+                    continue;
+                }
+                // 如果有队友在该队员前方则进行跟随并减速
+
+                // 寻找前方队友
+                var offset = otherMember.Position - member.Position;
+                var sign = Vector3.Dot(dir.normalized, offset.normalized);
+                if (sign > 0)
+                {
+                    // 前方有人
+                }
+                else
+                {
+                    // 否则全速前进不等队友
+                }
+
+            }
+            // 朝目标组队移动
+
+            // 计算与目标的相对向量
+
+            // 判断与队友的相对位置
+
+        }
+    }
+
+    /// <summary>
+    /// 清除所有组
+    /// </summary>
+    public static void ClearAllGroup()
+    {
+        GroupList = new List<SchoolGroup>();
+    }
+
+    /// <summary>
+    /// 根据ID查询group
+    /// </summary>
+    /// <param name="groupId">被查询groupId</param>
+    /// <returns>返回查询到的groupId 如果不存在则返回null</returns>
+    public static SchoolGroup GetGroupById(int groupId)
+    {
+        for (var i = 0; i < GroupList.Count; i++)
+        {
+            var tmpGroup = GroupList[i];
+            if (tmpGroup.GroupId == groupId)
+            {
+                return tmpGroup;
+            }
+        }
+        return null;
+    }
+
+}
